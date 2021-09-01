5be49f2d1fe70a0fcb1c836fc9fb9d3ca8cab3a8
Jawad63 <jawadqureshi111@gmail.com>
2021-09-01 10:16:52 +0200

    todo exercise complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React, { useState, useRef} from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef()
+
+
+// this is where we store our todos, so when we refresh the page our todos don't disappear:   
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+
+
+// this is where we get our todos:
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+
+// This is to store the change of our todo from being not complete to complete or vise versa:
+  
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -16,13 +43,18 @@ function App() {
     todoNameRef.current.value = null
   }
 
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList  todos = {todos} />
+      <TodoList  todos = {todos} toggleTodo = {toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Complete</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
